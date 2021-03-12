fd1269b452aa8effbf977708756504ed10224298
Amedeo Corsi <corsiamedeo@gmail.com>
2021-03-12 17:18:14 +0000

    fixed typo and removed unused stuff

--- server/index.ts
+++ server/index.ts
@@ -11,25 +11,15 @@ app.listen(8080, () => {
   console.log("Server running on port on port 8080");
 });
 
-interface DataPayload {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  gender: string;
-  country: string;
-  job_title: string;
-}
-
 app.get("/api/users", (req: Request, res: Response) => {
   const page = Number(req.query.page);
-  const payloadlenght = 30;
-  const start = page * payloadlenght - payloadlenght;
-  const end = page * payloadlenght;
+  const payloadLength = 30;
+  const start = page * payloadLength - payloadLength;
+  const end = page * payloadLength;
   const logsPayload = logs.slice(start, end);
   setTimeout(() => {
     res
       .status(200)
       .json({ hasNextPage: logsPayload.length > 1, logs: logsPayload });
-  }, 300);
+  }, 1000);
 });
